6ec1e326c0d71ecd62a3d93aff23e11f2060236a
ToastMasterson <tylercmasterson@gmail.com>
2019-12-10 11:47:45 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_01_173756) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "life_value"
